8ce045f346422277e405686e865abd710db8ea07
Muneer Alam <er.muneeralam.09@gmail.com>
2017-03-24 17:42:07 +0530

    all done

---
+++ views/component/video_craousel.react.js
@@ -0,0 +1,57 @@
+var React = require('react');
+var ReactBootstrap = require('react-bootstrap')
+import {Carousel,CarouselItem,CarouselCaption} from 'react-bootstrap';
+
+module.exports = React.createClass({
+
+getDefaultProps:function(){
+	return {video_data:{}}
+},
+getInitialState:function(){
+	return {index:0,direction:'next',data:{}}
+},
+handleSelect(selectedIndex, e) {
+	    //alert('selected=' + selectedIndex + ', direction=' + e.direction);
+	    this.setState({
+	      index: selectedIndex,
+	      direction: e.direction
+	    });
+	},
+
+getCraouselItem:function(){
+	var data = this.props.data;
+	console.log("adasds ",data)	;				
+				    										
+},
+render:function(){
+		
+		return (
+
+				<Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect}>
+			        {
+			        	<div>
+				        	data.map(function(row){
+							 console.log("asdasdsad ",row.id)       
+									
+									return (
+													<CarouselItem>
+														<img width="150" height="150" alt="150x150" src={row.thumbnailUrl}/>
+														<div className="carousel-caption">
+															<p>{row.title}</p>
+														</div>
+													</CarouselItem>
+											) 
+											
+							        
+							}.bind(this));
+			        	</div>
+
+			        }
+			    </Carousel>
+
+			);
+
+
+	}
+
+});
