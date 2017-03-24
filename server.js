f68df450cf09099ccc44da125ea40583f8a6495c
Muneer Alam <er.muneeralam.09@gmail.com>
2017-03-24 17:42:30 +0530

    chnages is done

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 
 var server = express();
+	, server = require('http').createServer(app)
+  	, io = io.listen(server);
 server.use(express.static(__dirname + '/src'));
 
 var port = 10001;
--- views/home.react.js
+++ views/home.react.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var AJAX = require('reqwest');
+var Videocraousel = require('./component/video_craousel.react');
 module.exports = React.createClass({
 	getInitialState:function(){
 		return {
@@ -11,24 +12,35 @@ module.exports = React.createClass({
 
 		var video_data = this.state.video_data;
 		var root = 'http://jsonplaceholder.typicode.com/photos';
+		var check_data_exist = JSON.parse(sessionStorage.getItem('video_data'));
+		if(check_data_exist == null){
 		AJAX({
                     url: root
                     , type: 'json'
                     , method:'get'
                     , success: function (resp) {
-                       
-                       console.log("data is ",resp); 
-                       
+                       sessionStorage.setItem('video_data',JSON.stringify(resp));
+                       this.setState({
+                       		video_data:resp
+                       }) 
                     }.bind(this)
                 }
             );
-
+		}else{
+			this.setState({video_data:check_data_exist});
+		}
 	},
 	render:function(){
-		var image = '../src/img/loader_image.gif'
+		var image = 'img/loader_image.gif';
+		var data = this.state.video_data;
+		//console.log("adasdas ",data);
 		return (
 					<div style={{'minHeight':'500px'}}>
-						<img src={image} className="image_c" />
+						{(data.length > 0) ?
+							<Videocraousel data={data}/>
+							:
+							<img src={image} className="image_c" />
+						}
 					</div>
 			);
 
